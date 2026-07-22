10568426ee79dd9372aaa714d2dbd24a9764d4d0
Izzat Ismail <mizzat1996@gmail.com>
2026-07-22 21:34:38 +0800

    core: First Init

---
+++ ios/MidTrim/MidTrim/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MidTrim
+//
+//  Created by Mohd Izzat Ismail Hashim on 22/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/MidTrim/MidTrim/MidTrimApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidTrimApp.swift
+//  MidTrim
+//
+//  Created by Mohd Izzat Ismail Hashim on 22/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct MidTrimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
